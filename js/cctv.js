8ce46b3b00edc92423293c74e802290393582aaa
pranavtelangade <pranavst29@gmail.com>
2022-10-06 21:39:24 +0530

    Add file preview

--- js/cctv.js
+++ js/cctv.js
@@ -4,3 +4,28 @@ $("#file-browser").click(function(e) {
   e.preventDefault();
   $("#file-upload").trigger("click");
 });
+
+document.querySelector("input[type=file]")
+  .onchange = function(event) {
+    var files = event.target.files;
+    for (var i = 0; i < files.length; i++) {
+      var f = files[i];
+      // Only process video files.
+      if (!f.type.match('video.*')) {
+        continue;
+      }
+
+      var source = document.createElement('video'); //added now
+
+      source.width = 280;
+
+      source.height = 240;
+
+      source.controls = true;
+
+      source.src = URL.createObjectURL(files[i]);
+
+      document.body.appendChild(source); // append `<video>` element
+
+    }
+  }
